dd4d96a80c6710caa0e7ecfb99442045eb971abd
Todd H <github@simplybest.com>
2024-09-23 22:49:09 -0600

    update, remove cap, remove zonejs

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'nocap-cord-health',
-  webDir: 'www'
-};
-
-export default config;
